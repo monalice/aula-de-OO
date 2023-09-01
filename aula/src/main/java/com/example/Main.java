a5c8b97493cd9dd0e9152b8553033c27965a6758
Alice Amorim <63621173+monalice@users.noreply.github.com>
2023-09-01 19:37:17 -0300

    corrigindo

--- aula/src/main/java/com/example/Main.java
+++ aula/src/main/java/com/example/Main.java
@@ -1,23 +1,15 @@
 package aula.src.main.java.com.example;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 import java.util.Scanner;
 
 
 public class Main {
     public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
-        Pattern patternCellphone = Pattern.compile("([0-9]{2})[0-9]{4}-[0-9]{4}");
-        Pattern patternPhone = Pattern.compile("([0-9]{2})[0-9]{5}-[0-9]{4}");
-
         System.out.println("Digite o seu telefone: ");
         String phone = teclado.nextLine();
-        Matcher matcherCellphone = patternCellphone.matcher(phone);
-        Matcher matcherPhone = patternPhone.matcher(phone);
-
-        boolean matchFoundCellphone = matcherCellphone.find();
-        boolean matchFoundPhone = matcherPhone.find();
+        boolean matcherPhone = phone.matcher("^\([0-9]{2}\)\s*[0-9]{4,5}-[0-9]{4}$");
+        
         if (matchFoundCellphone){
             System.out.println("celular: " + phone);
         } else if (matchFoundPhone){
