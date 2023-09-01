499d5fc4366c5cec976624d33f8505b73bd2e241
Alice Amorim <63621173+monalice@users.noreply.github.com>
2023-09-01 23:47:14 +0000

    exercicios

--- aula4/src/main/java/com/example/Main.java
+++ aula4/src/main/java/com/example/Main.java
@@ -1,11 +1,11 @@
-package aula.src.main.java.com.example;
+package aula4.src.main.java.com.example;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 import java.util.Scanner;
 
 
 public class Main {
     public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        //ler cinco pessoas com nome e idade e ordenar por esses dois parametros.
     }
 }
